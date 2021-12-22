62ca86a34a1c4351a0c9a865eb5ec962644fc51c
Janetta Kataoeva <jkattoeva@gmail.com>
2021-12-22 15:28:23 +0600

    added question in script

---
+++ script.js
@@ -0,0 +1,46 @@
+// var example1 = 10 === 10;   //eqauls
+// var example2 = 11 > 10;    //more than
+// var example3 = 10 < 11;  //less than
+// var example4 = 10 >= 10; //more than or equal
+// var example5 = 10 <= 10; //less than or eqaul
+// var example6 = 11 !== 10; //not eqauls
+
+// var a = 15;
+// var b = 10;
+// var example7 = a < b;
+
+
+// var c = 10;
+// var d = 11;
+// if (c < d) {
+//   alert("C is less than D")
+//   console.log('Hello World')
+// }
+
+// var e = 3.13;
+// if (e !== 3.14) { //false
+//   alert("Why don't you know number PI?")
+// }
+
+// else {
+//   alert('Good job!');
+// }
+
+// var age = 10;
+// if (age >= 18) {
+//   alert("You can vote!");
+// }
+// else {
+//   alert("You are too young to vote!"); //false 
+// }
+
+// var lightSwitch = 1;
+// if (lightSwitch === 1) {
+//   alert("Light is on")
+// }
+
+
+
+if (confirm("Do you have a account in Insragram?"))  {
+  alert("Oh no ")
+}
